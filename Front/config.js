9a66c9bf978395113936710d1fd511b00b8ea277
Vojin Minic <vojinminic@elfak.rs>
2023-12-22 13:55:12 +0100

    commit

--- Front/config.js
+++ Front/config.js
@@ -1,8 +1,3 @@
 
-<<<<<<< HEAD
-export const baseUrl = "http://10.66.98.32:5500";
-export const serverUrl = "http://10.66.98.32:5295"
-=======
 export const baseUrl = "http://192.168.28.8:5500";
 export const serverUrl = "http://192.168.28.8:5295";
->>>>>>> origin/lobby
